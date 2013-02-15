89bd5f632761f89e803617c1e66238626d71a9c6
Steve Jain <mfiver@gmail.com>
2013-02-14 22:42:33 -0500

    fix tabs on 1

--- pe1.py
+++ pe1.py
@@ -1,10 +1,10 @@
 def sumn(n):
-	num = 0
-	while num < n:
-		if ((num%3)==0) or ((num%5)==0):
-			print num	
-			yield num
-		num += 1
+    num = 0
+    while num < n:
+        if ((num%3)==0) or ((num%5)==0):
+            print num	
+            yield num
+        num += 1
 
 threefivesum = sum(sumn(1000))
 
